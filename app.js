59988312ef70dc2210a2fa2dfd16b09616d875cf
Ly Nguyen <Ly900@aol.com>
2019-04-05 22:59:22 -0400

    Start cash back data json file

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const CashCalculator = function() {
+	return(
+		<div className="cash-calc">
+			<CashCalcHeading/>
+			<CashCalcBody/>
+		</div>
+	);
+}
 const CashCalcHeading = function() {
 	return(
 		<div className="cash-calc__heading">
@@ -21,24 +29,30 @@ const CashCalcForm = function() {
 		<div className="cash-calc__block cash-calc__form-container">
 			<form className="cash-calc__form">
 
-				<label className="cash-calc__form-item">Buying or Selling</label>
-				<select className="cash-calc__form-item">
-					<option>Buying</option>
-					<option>Selling</option>
-					<option>Both</option>
-				</select>
+				<div className="cash-calc__input-section cash-calc__input-section__prompt">
+					<label className="cash-calc__form-item">Buying or Selling</label>
+					<select className="cash-calc__form-item">
+						<option value="Buying">Buying</option>
+						<option value="Selling">Selling</option>
+						<option value="Both">Both</option>
+					</select>
+				</div>
 
-				<label className="cash-calc__form-item">Purchase Price</label>
-				<select className="cash-calc__form-item">
-					<option>$10,000</option>
-					<option>$20,000</option>
-				</select>
+				<div className="cash-calc__input-section cash-calc__input-section_buying">
+					<label className="cash-calc__form-item">Purchase Price</label>
+					<select className="cash-calc__form-item">
+						<option>$10,000</option>
+						<option>$20,000</option>
+					</select>
+				</div>
 
-				<label className="cash-calc__form-item">Selling Price</label>
-				<select className="cash-calc__form-item">
-					<option>$10,000</option>
-					<option>$20,000</option>
-				</select>
+				<div className="cash-calc__input-section cash-calc__input-section_selling">
+					<label className="cash-calc__form-item cash-calc__selling">Selling Price</label>
+					<select className="cash-calc__form-item">
+						<option>$10,000</option>
+						<option>$20,000</option>
+					</select>
+				</div>
 
 			</form>
 		</div>
@@ -54,10 +68,7 @@ const CashCalcResults = function() {
 }
 
 ReactDOM.render(
-	<div className="cash-calc">
-		<CashCalcHeading/>
-		<CashCalcBody/>
-	</div>,
+	<CashCalculator/>,
 	document.getElementById("app")
 );
 
